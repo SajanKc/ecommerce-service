7f90ec8887b96eecad9860b9f16364e49f1984fa
Sajan Kc <sajan1996kc@gmail.com>
2022-06-14 09:52:01 +0545

    product and product category repository created

---
+++ src/main/java/com/iamsajan/ecommerce/repository/ProductCategoryRepository.java
@@ -0,0 +1,2 @@
+package com.iamsajan.ecommerce.repository;public interface ProductCategoryRepository {
+}
---
+++ src/main/java/com/iamsajan/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.iamsajan.ecommerce.repository;
+
+import com.iamsajan.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
