9903f779d36017556977806184b3c87ba23869a7
Sajan Kc <sajan1996kc@gmail.com>
2022-06-14 12:30:40 +0545

    corsorigin issue fixed

--- src/main/java/com/iamsajan/ecommerce/controller/ProductController.java
+++ src/main/java/com/iamsajan/ecommerce/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
